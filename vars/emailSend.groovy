34df0fc3a8a26d5639111e456c8ecdbeb118863c
Amin <miigook@github.com>
2025-03-22 11:43:43 -0500

    email one function

--- vars/emailSend.groovy
+++ vars/emailSend.groovy
@@ -1,11 +1,20 @@
-def call(SUCCESS,FAILURE){
-    if emailSendSuccess() == 'SUCCESS'{
-        emailext
+def call(String name){
+    if (currentBuild.result) == 'SUCCESS'{
+        emailext attachLog: true, 
+        body: "Pipeline ran successfully: ${JOB_URL}", 
+        subject: "shared Library:success job  ${JOB_NAME}, build number ${BUILD_NUMBER}", 
+            to: 'abdoovahap@gmail.com'
     }
-    else if (emailSendFailure() == "FAILURE"){
-        emailext
+    else if (currentBuild.result == "FAILURE"){
+        emailext attachLog: true, 
+        body: "Pipeline ran failedfailed ⛔️⛔️⛔️⛔️: ${JOB_URL}", 
+        subject: "shared Library:failed ⛔️⛔️⛔️⛔️ job  ${JOB_NAME}, build number ${BUILD_NUMBER}", 
+        to: 'abdoovahap@gmail.com'
     }
     else {
-        emailext
+        emailext attachLog: true, 
+        body: "Pipeline ran to unknown: ${JOB_URL}", 
+        subject: "unknown 🟡🟡🟡🟡 ${JOB_NAME}, build number ${BUILD_NUMBER}", 
+        to: 'abdoovahap@gmail.com'
     }
 }
